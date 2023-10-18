247d766bdc2bcf407a218bf017f2fe56b66afd94
PotterPlaysGames <105192939+PotterPlaysGames@users.noreply.github.com>
2023-10-17 20:50:00 -0400

    Added Andesite Powder; Quartz Powder; Vinyl Powder; Roll of Paper; Fabric Backing

---
+++ src/main/java/net/potterplaysgames/leatherplus/item/custom/StrapCutterItem.java
@@ -0,0 +1,9 @@
+package net.potterplaysgames.leatherplus.item.custom;
+
+import java.util.Properties;
+
+public class StrapCutterItem {
+    public StrapCutterItem(Properties pProperties){
+
+    }
+}
